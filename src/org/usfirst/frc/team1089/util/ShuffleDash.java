c5673e6aa68348779d27cb9c2b90197dd4998a43
Mercury 1089 <1089media@gmail.com>
2018-01-16 16:39:57 -0500

    Changed location of the Notifier for ShuffleDash

--- src/org/usfirst/frc/team1089/util/ShuffleDash.java
+++ src/org/usfirst/frc/team1089/util/ShuffleDash.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1089.util;
 import org.usfirst.frc.team1089.robot.Robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -10,6 +11,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class ShuffleDash {
+	public ShuffleDash() {
+		new Notifier(() -> updateDash()).startPeriodic(0.050);
+	}
 	public void updateDash() {
 		SmartDashboard.putString("FMS Data", DriverStation.getInstance().getGameSpecificMessage());
 		SmartDashboard.putString("Alliance Color", DriverStation.getInstance().getAlliance().toString());

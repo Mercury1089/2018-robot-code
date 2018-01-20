c233b1ed3da0cf294f834ed345537448e3fbe631
Jared Tulayan <2lyNJ.E@gmail.com>
2018-01-20 18:24:37 -0500

    Change sourceSets structure
    Changed it so /src is the root for both the
    main java sourceset and main resources sourceset.
    This is just for consistency with the ant WPILIBJ
    setup.

---
+++ src/org/usfirst/frc/team1089/robot/commands/DriveDistance.java
@@ -0,0 +1,92 @@
+package org.usfirst.frc.team1089.robot.commands;
+
+import org.usfirst.frc.team1089.robot.Robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1089.robot.subsystems.DriveTrain;
+
+/**
+ * Uses Talons and mag encoders to drive a set distance.
+ */
+public class DriveDistance extends Command {
+
+	private static double MOVE_THRESHOLD = 500;
+	private double distance;
+	private double percentVoltage; //Voltage is NOW from [-1, 1]
+    private double endPosL, endPosR;
+    private double waitTime;
+	
+    /**
+     * 
+     * @param distance in inches
+     * @param percentVoltage -1.0 to 1.0
+     */
+    public DriveDistance(double distance, double percentVoltage) {
+    	requires(Robot.driveTrain);
+    	this.distance = distance;
+    	this.percentVoltage = percentVoltage;
+    	endPosL = (distance / (Math.PI * Robot.driveTrain.WHEEL_DIAMETER_INCHES)) * Robot.driveTrain.MAG_ENCODER_TICKS_PER_REVOLUTION;
+    	endPosR = endPosL;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.driveTrain.resetEncoders();
+		Robot.driveTrain.disableTalonDrive();
+		Robot.driveTrain.getLeft().config_kP(DriveTrain.SLOT_0, .1, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getLeft().config_kI(DriveTrain.SLOT_0, 0, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getLeft().config_kD(DriveTrain.SLOT_0, .05, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().config_kP(DriveTrain.SLOT_0, .1, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().config_kI(DriveTrain.SLOT_0, 0, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().config_kD(DriveTrain.SLOT_0, .05, DriveTrain.TIMEOUT_MS);
+
+		Robot.driveTrain.getLeft().configNominalOutputForward(0, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getLeft().configNominalOutputReverse(0, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getLeft().configPeakOutputForward(percentVoltage, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getLeft().configPeakOutputReverse(-percentVoltage, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().configNominalOutputForward(0, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().configNominalOutputReverse(0, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().configPeakOutputForward(percentVoltage, DriveTrain.TIMEOUT_MS);
+		Robot.driveTrain.getRight().configPeakOutputReverse(-percentVoltage, DriveTrain.TIMEOUT_MS);
+
+		System.out.println(endPosL);
+		Robot.driveTrain.getLeft().set(ControlMode.Position, endPosL);
+		Robot.driveTrain.getRight().set(ControlMode.Position, endPosR);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        boolean isFinished = false;
+        
+        double leftPos = Robot.driveTrain.getLeft().getSelectedSensorPosition(DriveTrain.PRIMARY_PID_LOOP);
+        //System.out.println(leftPos);
+        double rightPos = Robot.driveTrain.getRight().getSelectedSensorPosition(DriveTrain.PRIMARY_PID_LOOP);
+        if ((leftPos > endPosL - MOVE_THRESHOLD && leftPos < endPosL + MOVE_THRESHOLD)
+				&& (rightPos > endPosR - MOVE_THRESHOLD && rightPos < endPosR + MOVE_THRESHOLD)) {
+   			isFinished = true;
+   			System.out.println(" I'm done");
+   		}
+
+    	return isFinished;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.stop();
+    	Robot.driveTrain.enableTalonDrive();
+    	Robot.driveTrain.resetEncoders();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	System.out.println(" I'm interrupted");
+    }
+}
---
+++ src/org/usfirst/frc/team1089/robot/commands/DriveTank.java
@@ -0,0 +1,67 @@
+package org.usfirst.frc.team1089.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.usfirst.frc.team1089.robot.Robot;
+import org.usfirst.frc.team1089.robot.RobotMap.DS_USB;
+import org.usfirst.frc.team1089.util.DelayableLogger;
+import org.usfirst.frc.team1089.util.TalonDrive;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Command that puts the drive train into a manual control mode.
+ * This puts the robot in tank drive.
+ */
+public class DriveTank extends Command {
+	private TalonDrive tDrive;
+	private Logger log = LogManager.getLogger();
+	private DelayableLogger everySecond = new DelayableLogger(log, 1_000, TimeUnit.MILLISECONDS);
+	//TODO: think of better naming convention for InfrequentLogger
+
+	public DriveTank() {
+		requires(Robot.driveTrain);
+		setName("DriveTank Command");
+		log.debug(getName() + " command created");
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		tDrive = Robot.driveTrain.getTalonDrive();
+		log.info(getName() + " command initialized");
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		if (tDrive != null) {
+			tDrive.tankDrive(Robot.oi.getMoveValue(DS_USB.LEFT_STICK), Robot.oi.getMoveValue(DS_USB.RIGHT_STICK));
+		} else {
+			System.out.println("Talon Drive is not initialized!");
+		}
+		everySecond.run(log -> log.info("tank driving"));
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		//Robot.driveTrain.stop();
+		System.out.println("TankDrive has ended");
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		log.info("interrupted");
+		end();
+	}
+}

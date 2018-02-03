c9983608b954111fe78a906cdcc8e6f1f60eb23e
DrewTheRat <HappyJ0y!>
2018-02-03 11:42:06 -0500

    Added Pixy classes from https://github.com/croadfeldt/wpilib_pixy_spi_java

---
+++ robot/src/org/usfirst/frc/team1089/robot/sensors/PixyException.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team1089.robot.sensors;
+
+
+
+	public class PixyException extends Exception{
+		public PixyException(String message){
+			super(message);
+		}
+}
+
+
---
+++ robot/src/org/usfirst/frc/team1089/robot/sensors/PixyPacket.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1089.robot.sensors;
+
+
+/**
+ *
+ */
+public class PixyPacket {
+	public int X;
+	public int Y;
+	public int Width;
+	public int Height;
+	public int checksumError;
+}

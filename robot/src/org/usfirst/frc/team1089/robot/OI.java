42ce1e6fb4222a0bdb114a65e035f5c7bfc97689
Mercury 1089 <1089media@gmail.com>
2018-02-03 14:58:56 -0500

    Made Elevator and UseElevator classes

--- robot/src/org/usfirst/frc/team1089/robot/OI.java
+++ robot/src/org/usfirst/frc/team1089/robot/OI.java
@@ -84,7 +84,7 @@ public class OI {
 
 		// Right stick button binds
 		right1 = new JoystickButton(rightStick,1);
-		right1.whenPressed(new MoveOnPath("SwitchMidRight"));
+		right1.whenPressed(new MoveOnPath("SwitchFrontRight"));
 
 		// Gamepad button binds
 		gamepad_a = new JoystickButton(gamepad, 1);

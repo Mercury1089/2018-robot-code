4e2977299bbfbed51bea91fc5d9dc67c48e8e64a
Srihari S <srihari_subramanian@yahoo.com>
2018-02-06 20:12:16 -0500

    Auton logic. AutonCommand has some good code

---
+++ robot/src/org/usfirst/frc/team1089/robot/auton/InitialMiddleSwitchSide.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team1089.robot.auton;
+
+public enum InitialMiddleSwitchSide {
+    LEFT_MID,
+    RIGHT_MID;
+}
--- robot/src/org/usfirst/frc/team1089/robot/commands/GetCube.java
+++ robot/src/org/usfirst/frc/team1089/robot/commands/GetCube.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1089.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team1089.robot.Robot;
 
 /**
  * Command group that calls both the AutoAlign command
@@ -8,8 +9,18 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  * approach the cube.
  */
 public class GetCube extends CommandGroup {
+    private double angleTurned, distanceTraveled;
     public GetCube() {
+        angleTurned = Robot.camera.getAngleFromCube();
         addSequential(new AutoAlign());
+        distanceTraveled = Robot.manipulator.getLidar().getDistance();
         addSequential(new DriveWithLIDAR(2, .3));
+    }
+    public double getAngleTurned() {
+        return angleTurned;
+    }
+
+    public double getDistanceTraveled() {
+        return distanceTraveled;
     }
 }

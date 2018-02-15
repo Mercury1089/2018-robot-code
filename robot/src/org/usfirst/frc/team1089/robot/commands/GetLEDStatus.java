f748379e32e21bfa9dc762074c86cc76e2997578
Team Mercury 1089 <1089software@gmail.com>
2018-02-15 17:54:20 -0500

    #6 Move GetCubeStatus code to Claw.execute()

--- robot/src/org/usfirst/frc/team1089/robot/commands/GetLEDStatus.java
+++ robot/src/org/usfirst/frc/team1089/robot/commands/GetLEDStatus.java
@@ -2,44 +2,29 @@ package org.usfirst.frc.team1089.robot.commands;
 
 import com.ctre.phoenix.CANifier;
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc.team1089.robot.OI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.usfirst.frc.team1089.robot.Robot;
+import org.usfirst.frc.team1089.util.DelayableLogger;
+
+import java.util.concurrent.TimeUnit;
 
 public class GetLEDStatus extends Command {
-    public final double MIN_INCHES = 8.0;
+    private final Logger LOG = LogManager.getLogger(GetLEDStatus.class);
+    private final DelayableLogger SLOW_LOG = new DelayableLogger(LOG, 5, TimeUnit.SECONDS);
+
 
     public GetLEDStatus() {
         requires(Robot.ledIndicators);
     }
 
     @Override
-    protected void execute () {
-        if (Robot.vision.getPixyCam().inRange()) { //Cube is in range to auto pickup
-            //set green (R: 28.2%, G: 91%, B: 14.9%)
-            Robot.claw.getCanifier().setLEDOutput(28.2, CANifier.LEDChannel.LEDChannelA);
-            Robot.claw.getCanifier().setLEDOutput(91, CANifier.LEDChannel.LEDChannelB);
-            Robot.claw.getCanifier().setLEDOutput(14.9, CANifier.LEDChannel.LEDChannelC);
+    protected void initialize() {
+        LOG.info("GetLEDStatus initialized");
+    }
 
-            Robot.oi.rumbleController(true);
-        } else if (Robot.claw.getLidar().getDistance() <= MIN_INCHES) { //Have cube?
-            //set orange (R: 100%, G: 74.5%, B: 3.9%)
-            Robot.claw.getCanifier().setLEDOutput(100, CANifier.LEDChannel.LEDChannelA);
-            Robot.claw.getCanifier().setLEDOutput(74.5, CANifier.LEDChannel.LEDChannelB);
-            Robot.claw.getCanifier().setLEDOutput(3.9, CANifier.LEDChannel.LEDChannelC);
-            Robot.oi.rumbleController(false);
-        } else if (Robot.claw.getEjecting()) { //Robo is ejecting cube
-            //set red (R: 100%, G: 3.9%, B: 3.9%)
-            Robot.claw.getCanifier().setLEDOutput(100, CANifier.LEDChannel.LEDChannelA);
-            Robot.claw.getCanifier().setLEDOutput(3.9, CANifier.LEDChannel.LEDChannelB);
-            Robot.claw.getCanifier().setLEDOutput(3.9, CANifier.LEDChannel.LEDChannelC);
-            Robot.oi.rumbleController(false);
-        } else { //default case
-            //set white (All values max power, maximum)
-            Robot.claw.getCanifier().setLEDOutput(100, CANifier.LEDChannel.LEDChannelA);
-            Robot.claw.getCanifier().setLEDOutput(100, CANifier.LEDChannel.LEDChannelB);
-            Robot.claw.getCanifier().setLEDOutput(100, CANifier.LEDChannel.LEDChannelC);
-            Robot.oi.rumbleController(false);
-        }
+    @Override
+    protected void execute () {
     }
 
     @Override

616e2aff6b3357c37aab9d1c27ca477779dd9564
Sagar Patel <sagarpatel201@gmail.com>
2018-02-19 09:44:33 -0500

    #25 Fixed ScoringSide Enum.

--- auton-app/src/org/usfirst/frc/team1089/main/ScoringSide.java
+++ auton-app/src/org/usfirst/frc/team1089/main/ScoringSide.java
@@ -11,29 +11,31 @@ public enum ScoringSide {
     public static final StringConverter<ScoringSide> STRING_CONVERTER = new StringConverter<ScoringSide>() {
         @Override
         public String toString(ScoringSide object) {
-            if (object == null) {
-                return "";
-            }
             switch (object) {
                 case NOT_APPLICABLE:
                     return "Not Applicable";
+                case FRONT:
+                    return "Front";
+                case MID:
+                    return "Mid";
+                case BACK:
+                    return "Back";
                 default:
-                    return object.toString();
+                    return "";
             }
         }
 
         @Override
         public ScoringSide fromString(String string) {
             switch (string) {
-                case "FRONT": {
+                case "Front":
                     return ScoringSide.FRONT;
-                }
-                case "Middle": {
+                case "Middle":
                     return ScoringSide.MID;
-                }
-                case "BACK": {
+                case "Back":
                     return ScoringSide.BACK;
-                }
+                case "Not Applicable":
+                    return NOT_APPLICABLE;
                 default:
                     return null;
             }

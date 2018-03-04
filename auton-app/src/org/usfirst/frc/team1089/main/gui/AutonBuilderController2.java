c754cc21cc76e8a5370652ec177efe5776708a21
Jared Tulayan <2lyNJ.E@gmail.com>
2018-03-04 18:32:42 -0500

    Edit AutonBuilderController2 for readability

--- auton-app/src/org/usfirst/frc/team1089/main/gui/AutonBuilderController2.java
+++ auton-app/src/org/usfirst/frc/team1089/main/gui/AutonBuilderController2.java
@@ -130,9 +130,10 @@ public class AutonBuilderController2 {
         Callback<TableColumn<TaskConfig, TaskConfig.AutonTask>, TableCell<TaskConfig, TaskConfig.AutonTask>> autonTaskCellFactory = param -> {
             final TransparentComboBoxTableCell<TaskConfig, TaskConfig.AutonTask> comboBoxTableCell = new TransparentComboBoxTableCell<>(FXCollections.observableArrayList(TaskConfig.AutonTask.values()));
             comboBoxTableCell.setEditable(true);
-            //ComboBoxTableCell does not have an itemsproperty for the list of items that can be displayed. However, ComboBox does.
-            //Using the TransparentComboBoxTableCell, we can access the ComboBox used in the ComboBoxTableCell.
-            //So, we can use bindings to change the items displayed in the ComboBox.
+
+            // ComboBoxTableCell does not have an items property for the list of items that can be displayed. However, ComboBox does.
+            // Using the TransparentComboBoxTableCell, we can access the ComboBox used in the ComboBoxTableCell.
+            // So, we can use bindings to change the items displayed in the ComboBox.
             comboBoxTableCell.comboBox.itemsProperty().bind(Bindings
                     .when(comboBoxTableCell.indexProperty().isEqualTo(0))
                     .then(new SimpleObjectProperty<>(FXCollections.observableArrayList(TaskConfig.AutonTask.values())))
@@ -142,24 +143,28 @@ public class AutonBuilderController2 {
             return comboBoxTableCell;
         };
 
-        Callback<TableColumn<TaskConfig, TaskConfig.ScoringSide>, TableCell<TaskConfig, TaskConfig.ScoringSide>> scoringSideCellFactory = param -> {
-            ComboBoxTableCell<TaskConfig, TaskConfig.ScoringSide> comboBoxTableCell = new ComboBoxTableCell<>(TaskConfig.ScoringSide.FRONT, TaskConfig.ScoringSide.MID, TaskConfig.ScoringSide.BACK);
-            comboBoxTableCell.setComboBoxEditable(false);
-            comboBoxTableCell.editableProperty().bind(comboBoxTableCell.itemProperty().isNotEqualTo(new SimpleObjectProperty<>(TaskConfig.ScoringSide.NOT_APPLICABLE)));
-            return comboBoxTableCell;
-        };
+        Callback<TableColumn<TaskConfig, TaskConfig.ScoringSide>, TableCell<TaskConfig, TaskConfig.ScoringSide>>
+                scoringSideCellFactory = param -> {
+                    ComboBoxTableCell<TaskConfig, TaskConfig.ScoringSide> comboBoxTableCell = new ComboBoxTableCell<>(TaskConfig.ScoringSide.FRONT, TaskConfig.ScoringSide.MID, TaskConfig.ScoringSide.BACK);
 
-        EventHandler<TableColumn.CellEditEvent<TaskConfig,TaskConfig.AutonTask>> taskEditHandler = (TableColumn.CellEditEvent<TaskConfig, TaskConfig.AutonTask> t) -> {
-            if (t.getNewValue() == TaskConfig.AutonTask.AUTO_LINE) {
-                t.getTableView().setItems(FXCollections.observableArrayList(new TaskConfig(TaskConfig.AutonTask.AUTO_LINE, TaskConfig.ScoringSide.NOT_APPLICABLE)));
-            }
+                    comboBoxTableCell.setComboBoxEditable(false);
+                    comboBoxTableCell.editableProperty().bind(comboBoxTableCell.itemProperty().isNotEqualTo(new SimpleObjectProperty<>(TaskConfig.ScoringSide.NOT_APPLICABLE)));
 
-            if (t.getOldValue() == TaskConfig.AutonTask.AUTO_LINE && t.getNewValue() != TaskConfig.AutonTask.AUTO_LINE) {
-                t.getRowValue().scoringSide.setValue(TaskConfig.ScoringSide.FRONT);
-            }
+                    return comboBoxTableCell;
+                };
 
-            t.getRowValue().autonTask.setValue(t.getNewValue());
-        };
+        EventHandler<TableColumn.CellEditEvent<TaskConfig,TaskConfig.AutonTask>>
+                taskEditHandler = (TableColumn.CellEditEvent<TaskConfig, TaskConfig.AutonTask> t) -> {
+                    if (t.getNewValue() == TaskConfig.AutonTask.AUTO_LINE) {
+                        t.getTableView().setItems(FXCollections.observableArrayList(new TaskConfig(TaskConfig.AutonTask.AUTO_LINE, TaskConfig.ScoringSide.NOT_APPLICABLE)));
+                    }
+
+                    if (t.getOldValue() == TaskConfig.AutonTask.AUTO_LINE && t.getNewValue() != TaskConfig.AutonTask.AUTO_LINE) {
+                        t.getRowValue().scoringSide.setValue(TaskConfig.ScoringSide.FRONT);
+                    }
+
+                    t.getRowValue().autonTask.setValue(t.getNewValue());
+                };
 
         Callback<TableColumn.CellDataFeatures<TaskConfig, TaskConfig.AutonTask>, ObservableValue<TaskConfig.AutonTask>>
                 taskValueFactory = param -> param.getValue().autonTask;

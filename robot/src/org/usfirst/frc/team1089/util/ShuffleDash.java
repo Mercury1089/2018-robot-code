2f7e876b9e5d837d2937a9535218cffadf3bd9f2
Mercury 1089 <1089media@gmail.com>
2018-03-09 15:28:03 -0500

    Shuffleboard finalized?

--- robot/src/org/usfirst/frc/team1089/util/ShuffleDash.java
+++ robot/src/org/usfirst/frc/team1089/util/ShuffleDash.java
@@ -18,26 +18,25 @@ public class ShuffleDash {
     }
 
     private void updateDash() {
-        SmartDashboard.putString("Alliance Color", DriverStation.getInstance().getAlliance().toString());
-        if (Robot.driveTrain.getGyro() != null)
-            SmartDashboard.putNumber("NavX Angle", Robot.driveTrain.getGyro().getAngle());
+        //SmartDashboard.putString("Alliance Color", DriverStation.getInstance().getAlliance().toString());
 
-        SmartDashboard.putNumber("Left Enc in ticks", Robot.driveTrain.getLeft().getSelectedSensorPosition(DriveTrain.PRIMARY_PID_LOOP));
+        /*SmartDashboard.putNumber("Left Enc in ticks", Robot.driveTrain.getLeft().getSelectedSensorPosition(DriveTrain.PRIMARY_PID_LOOP));
         SmartDashboard.putNumber("Right Enc in ticks", Robot.driveTrain.getRight().getSelectedSensorPosition(DriveTrain.PRIMARY_PID_LOOP));
         SmartDashboard.putNumber("Left Enc in feet", Robot.driveTrain.getLeftEncPositionInFeet());
         SmartDashboard.putNumber("Right Enc in feet", Robot.driveTrain.getRightEncPositionInFeet());
-        SmartDashboard.putNumber("Elevator Enc in ticks", Robot.elevator.getCurrentHeight());
-        SmartDashboard.putBoolean("Elevator Limit Switch Closed", Robot.elevator.isLimitSwitchClosed());
-        SmartDashboard.putBoolean("Has Cube", Robot.claw.hasCube());
+        SmartDashboard.putNumber("Elevator Enc in ticks", Robot.elevator.getCurrentHeight());*/
+        SmartDashboard.putBoolean("Limit Switch Closed", Robot.elevator.isLimitSwitchClosed());
+        SmartDashboard.putBoolean("Cube Acquired", Robot.claw.hasCube());
+        SmartDashboard.putBoolean("Cube Acquirable", Robot.claw.getPixyCam().inRange());
         SmartDashboard.putString("DriveTrain", Robot.driveTrain.getCurrentCommandName());
-        SmartDashboard.putNumber("Left Wheel RPM", MercMath.ticksPerTenthToRevsPerMinute(Robot.driveTrain.getLeft().getSelectedSensorVelocity(DriveTrain.PRIMARY_PID_LOOP))); //ticks per tenth of a second
-        SmartDashboard.putNumber("Right Wheel RPM", MercMath.ticksPerTenthToRevsPerMinute(Robot.driveTrain.getRight().getSelectedSensorVelocity(DriveTrain.PRIMARY_PID_LOOP)));
-        SmartDashboard.putNumber("LIDAR Raw Distance (in.)", MercMath.roundFloat(Robot.claw.getLidar().getRawDistance(), 10));
+        //SmartDashboard.putNumber("Left Wheel RPM", MercMath.ticksPerTenthToRevsPerMinute(Robot.driveTrain.getLeft().getSelectedSensorVelocity(DriveTrain.PRIMARY_PID_LOOP))); //ticks per tenth of a second
+        //SmartDashboard.putNumber("Right Wheel RPM", MercMath.ticksPerTenthToRevsPerMinute(Robot.driveTrain.getRight().getSelectedSensorVelocity(DriveTrain.PRIMARY_PID_LOOP)));
+        //SmartDashboard.putNumber("LIDAR Raw Distance (in.)", MercMath.roundFloat(Robot.claw.getLidar().getRawDistance(), 10));
         SmartDashboard.putNumber("LIDAR Distance (in.)", MercMath.roundFloat(Robot.claw.getLidar().getDistance(), 10));
-        SmartDashboard.putNumber("LIDAR Period", MercMath.roundFloat(Robot.claw.getLidar().getDistance(), 10));
+        //SmartDashboard.putNumber("LIDAR Period", MercMath.roundFloat(Robot.claw.getLidar().getDistance(), 10));
         SmartDashboard.putNumber("Gyro Angle", Robot.driveTrain.getGyro().getAngle());
         SmartDashboard.putNumber("Ultrasonic Distance", Robot.claw.getUltrasonic().getRange());
         SmartDashboard.putNumber("Pixy Displacement", Robot.claw.getPixyCam().pidGet());
-        SmartDashboard.putString("LED Output",Robot.claw.getCurrentLEDOutput()[0]+","+Robot.claw.getCurrentLEDOutput()[1]+","+Robot.claw.getCurrentLEDOutput()[2]);
+        //SmartDashboard.putString("LED Output",Robot.claw.getCurrentLEDOutput()[0]+","+Robot.claw.getCurrentLEDOutput()[1]+","+Robot.claw.getCurrentLEDOutput()[2]);
     }
 }
